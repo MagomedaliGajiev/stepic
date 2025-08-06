3c295bead7f6cf13559188daf0125a71e33f2046
Magomedali Gajiev <mag198421@gmail.com>
2025-08-07 00:48:42 +0300

    Добавил методы

---
+++ stepic/EF/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using stepic.Models;
+
+namespace stepic.EF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
---
+++ stepic/EF/UsersService.cs
@@ -0,0 +1,69 @@
+﻿using stepic.Models;
+
+namespace stepic.EF
+{
+    public class UsersService
+    {
+        /// <summary>
+        /// Получение пользователя из таблицы users
+        /// </summary>
+        /// <param name="fullName">Полное имя пользователя</param>
+        /// <returns>User</returns>
+        public User? Get(string fullName)
+        {
+            using ApplicationDbContext dbContext = new();
+            
+            return dbContext.Users
+                .FirstOrDefault(u => u.FullName == fullName && u.IsActive);
+        }
+
+        /// <summary>
+        /// Получение общего количества пользователей
+        /// </summary>
+        public int GetTotalCount()
+        {
+            using ApplicationDbContext dbContext = new();
+            return dbContext.Users.Count(); 
+        }
+
+            /// <summary>
+            /// Добавление нового пользователя в таблицу users
+            /// </summary>
+            /// <param name="user">Новый пользователь</param>
+            /// <returns>Удалось ли добавить пользователя</returns>
+            public bool Add(User user)
+            {
+                try
+                {
+                    using ApplicationDbContext dbContext = new();
+                    dbContext.Users.Add(user);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+        /// <summary>
+        /// Форматирование показателей пользователя
+        /// </summary>
+        /// <param name="number">Число для форматирования</param>
+        /// <returns>Отформатированное число</returns>
+        public string? FormatUserMetrics(int number)
+        {
+            if (number < 1000)
+            {
+                return number.ToString();
+            }
+            else
+            {
+                double thousands = number / 1000.0;
+                var formatted = thousands.ToString("0.0", System.Globalization.CultureInfo.InvariantCulture)
+                        + "K";
+
+                return formatted.Replace(".0K", "K");
+            }
+        }
+    }
+}
