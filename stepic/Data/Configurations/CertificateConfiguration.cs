cd878fc28bbd8e52afbaa3f5969d2fb2a299e2ab
Магомедали Гаджиев <mag198421@gmail.com>
2025-08-16 18:08:30 +0300

    Добавил CertificateConfiguration

---
+++ stepic/Data/Configurations/CertificateConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using stepic.Models;
+
+namespace stepic.Data.Configurations;
+
+public class CertificateConfiguration : IEntityTypeConfiguration<Certificate>
+{
+    public void Configure(EntityTypeBuilder<Certificate> builder)
+    {
+        builder.ToTable("certificates");
+
+        builder.HasKey(c => new { c.UserId, c.CourseId });
+
+        builder.Property(c => c.UserId)
+            .HasColumnName("user_id");
+
+        builder.Property(c => c.CourseId)
+            .HasColumnName("course_id");
+
+        builder.Property(c => c.Grade)
+            .HasColumnName("grade");
+
+        builder.Property(c => c.IssueDate)
+            .HasColumnName("issue_date");
+
+        builder.Property(c => c.Url)
+            .HasColumnName("url");
+
+        builder.HasOne(c => c.User)
+            .WithMany(u => u.Certificates)
+            .HasForeignKey(c => c.UserId);
+
+        builder.HasOne(c => c.Course)
+            .WithMany(course => course.Certificates)
+            .HasForeignKey(c => c.CourseId);
+    }
+}
--- stepic/Models/Certificate.cs
+++ stepic/Models/Certificate.cs
@@ -1,25 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace stepic.Models;
 
-namespace stepic.Models;
-
-[PrimaryKey(nameof(UserId), nameof(CourseId))]
-[Table("certificates")]
 public class Certificate
 {
-    [Column("user_id")]
     public int UserId { get; set; }
 
-    [Column("course_id")]
     public int CourseId { get; set; }
 
-    [Column("grade")]
     public int Grade { get; set; }
 
-    [Column("issue_date")]
     public DateTime IssueDate { get; set; }
 
-    [Column("url")]
     public string Url { get; set; }
 
     public User User { get; set; }
